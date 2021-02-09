31a38615ef9245cc788299d2d1606321dfb28360
rinatsp <rinat95.95@yandex.ru>
2021-02-09 23:56:40 +0300

    1 file modified delete hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import {ApplePhoneFormModule} from "./apple-phone-form/apple-phone-form.module";
+import { AppComponent } from "./app.component";
+import { ApplePhoneFormModule } from "./apple-phone-form/apple-phone-form.module";
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, ApplePhoneFormModule],
-    declarations: [ AppComponent, HelloComponent ],
-    bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ApplePhoneFormModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
